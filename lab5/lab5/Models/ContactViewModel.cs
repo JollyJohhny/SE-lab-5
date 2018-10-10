f24bcd8a9dfb38f9dd604e8bee9fd8d6abb6dbd6
Junaid Ali <johhny424@gmail.com>
2018-10-10 14:03:36 +0500

    complete

---
+++ lab5/lab5/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab5.Models
+{
+    public class ContactViewModel
+    {
+        public int ContactId { get; set; }
+        
+        //[RegularExpression(@"")]
+        public string ContactNumber { get; set; }
+        [RegularExpression(@"^[a-z -']+$")]
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+
+    }
+}
---
+++ lab5/lab5/Models/PersonViewModel.vb
@@ -0,0 +1,3 @@
+﻿Public Class PersonViewModel
+
+End Class
